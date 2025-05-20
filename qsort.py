3a6f232ae0b50d0b54b16ba6a233c2ef5998ef62
Stupnikov Dmitry <d_lis82@mail.ru>
2025-05-20 16:51:20 +0300

    готово

--- qsort.py
+++ qsort.py
@@ -1,4 +1,7 @@
 from random import randint
+import timeit
+
+# Быстрая сортировка
 
 def qsort(arr):
     if len(arr) <=1:
@@ -17,6 +20,20 @@ def qsort(arr):
                 middle.append(i)
         return qsort(left) + middle + qsort(right)
 
+# Сортировка вставками
+
+def insertion(arr):
+    for i in range(1, len(arr)):
+        item = arr[i]
+        i2 = i - 1
+        while i2 >= 0 and arr[i2] > item:
+            arr[i2 + 1] = arr[i2]
+            i2 -= 1
+            arr[i2 + 1] = item
+
+    return arr
+
+
 thousand = []                   # список из 1000 чисел
 for i in range(1000):
     thousand.append(randint(0, 10000))
@@ -28,6 +45,25 @@ for i in range(100):
 ten = []
 for i in range(10):
     ten.append(randint(0, 100))
-arr = thousand
-print(arr)
-print(qsort(arr))
+
+
+
+def qsearch():
+    arr =  thousand             # сюда подставляем все списки
+    qsort(arr)
+
+def isearch():
+    arr =  thousand             # сюда подставляем все списки
+    insertion(arr)
+
+print(timeit.timeit(stmt = "qsearch()", globals=globals(), number = 1000))
+print(timeit.timeit(stmt = "isearch()", globals=globals(), number = 1000))
+
+# Сравнение времени выполнения на разных длинах:
+# qsort, длина 10 элементов - 0,003
+# insertion, длина 10 элементов - 0,0005
+# qsort, длина 100 элементов - 0,044
+# insertion, длина 100 элементов - 0,0047
+# qsort, длина 1000 элементов - 0,63
+# insertion, длина 1000 элементов - 0,069
+
